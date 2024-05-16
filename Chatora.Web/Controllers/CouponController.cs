8f4f229ffd0bbd5bad246509b917e270833fa5ea
curs3m4rk <pujyapreyans35@gmail.com>
2024-05-17 01:01:55 +0530

    Coupon CRUD | Added Coupon Controller and CouponIndex to Display Coupons on Page

---
+++ Chatora.Web/Controllers/CouponController.cs
@@ -0,0 +1,30 @@
+using Chatora.Web.Models;
+using Chatora.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Chatora.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDto>? list = new();
+
+            ResponseDto? response = await _couponService.GetAllCouponsAsync();
+
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+
+    }
+}
