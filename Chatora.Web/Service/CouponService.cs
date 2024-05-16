b30133f3e18a9be281bcb078ef49840539da1565
curs3m4rk <pujyapreyans35@gmail.com>
2024-05-16 23:28:49 +0530

    Coupon CRUD | Added CouponService, Added Dependency Injection for IBaseService, ICouponService, HttpClient and CouponService BaseURL in Appsettings

---
+++ Chatora.Web/Service/CouponService.cs
@@ -0,0 +1,44 @@
+using Chatora.Web.Models;
+using Chatora.Web.Service.IService;
+
+namespace Chatora.Web.Service;
+
+public class CouponService : ICouponService
+{
+    private readonly IBaseService _baseService;
+
+    public CouponService(IBaseService baseService)
+    {
+        _baseService = baseService;
+    }
+
+    public Task<ResponseDto?> GetAllCouponsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseDto?> GetCouponAsync(string couponCode)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseDto?> GetCouponByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseDto?> CreateCouponsAsync(CouponDto couponDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseDto?> UpdateCouponsAsync(CouponDto couponDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseDto?> DeleteCouponsAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
