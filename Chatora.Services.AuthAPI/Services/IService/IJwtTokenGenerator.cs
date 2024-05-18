015e12add57e9135b78529dc5aa180968bb329f3
curs3m4rk <pujyapreyans35@gmail.com>
2024-05-18 17:07:36 +0530

    Auth API | JWT Token Generator

---
+++ Chatora.Services.AuthAPI/Services/IService/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using Chatora.Services.AuthAPI.Models;
+
+namespace Chatora.Services.AuthAPI.Services.IService
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(ApplicationUser applicationUser);
+    }
+}
