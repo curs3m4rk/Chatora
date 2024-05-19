33af39d8a475e5a1b33f2bd0e424928016df4120
curs3m4rk <pujyapreyans35@gmail.com>
2024-05-19 23:52:05 +0530

    Auth API | Auth Service in Web Project

---
+++ Chatora.Web/Service/AuthService.cs
@@ -0,0 +1,46 @@
+﻿using Chatora.Web.Models;
+using Chatora.Web.Models.Dto;
+using Chatora.Web.Service.IService;
+using Chatora.Web.Utility;
+
+namespace Chatora.Web.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IBaseService _baseService;
+
+        public AuthService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+        public async Task<ResponseDto?> AssignRoleAsync(RegistrationRequestDto registrationRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = registrationRequestDto,
+                Url = SD.AuthAPIBase + "/api/auth/AssignRole"
+            });
+        }
+
+        public async Task<ResponseDto?> LoginAsync(LoginRequestDto loginRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = loginRequestDto,
+                Url = SD.AuthAPIBase + "/api/auth/login"
+            });
+        }
+
+        public async Task<ResponseDto?> RegisterAsync(RegistrationRequestDto registrationRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = registrationRequestDto,
+                Url = SD.AuthAPIBase + "/api/auth/register"
+            });
+        }
+    }
+}
---
+++ Chatora.Web/Service/IService/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using Chatora.Web.Models;
+using Chatora.Web.Models.Dto;
+
+namespace Chatora.Web.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<ResponseDto?> RegisterAsync(RegistrationRequestDto registrationRequestDto);
+        Task<ResponseDto?> LoginAsync(LoginRequestDto loginRequestDto);
+        Task<ResponseDto?> AssignRoleAsync(RegistrationRequestDto registrationRequestDto);
+    }
+}
