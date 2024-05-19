15763b0ef784a5a708e62920d1edfe2ad6ade8a4
curs3m4rk <pujyapreyans35@gmail.com>
2024-05-20 00:37:34 +0530

    Auth API | dropdown for Role in Register form using SD class.

--- Chatora.Web/Controllers/AuthController.cs
+++ Chatora.Web/Controllers/AuthController.cs
@@ -1,6 +1,8 @@
-﻿using Chatora.Web.Models;
+﻿using Chatora.Web.Models    ;
 using Chatora.Web.Service.IService;
+using Chatora.Web.Utility;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Chatora.Web.Controllers
 {
@@ -23,6 +25,13 @@ namespace Chatora.Web.Controllers
         [HttpGet]
         public IActionResult Register()
         {
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem{Text=SD.RoleAdmin,Value=SD.RoleAdmin},
+                new SelectListItem{Text=SD.RoleCustomer,Value=SD.RoleCustomer}
+            };
+            ViewBag.RoleList = roleList;
+
             return View();
         }
 
