cdfd3ca652706f43152ec384e04af7100446269e
curs3m4rk <pujyapreyans35@gmail.com>
2024-05-20 22:40:17 +0530

    Auth API | Register User & Role Implementation

--- Chatora.Web/Controllers/AuthController.cs
+++ Chatora.Web/Controllers/AuthController.cs
@@ -35,6 +35,37 @@ namespace Chatora.Web.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationRequestDto obj)
+        {
+            ResponseDto result = await _authService.RegisterAsync(obj);
+            ResponseDto assignRole;
+
+            if(result != null && result.IsSuccess)
+            {
+                if(string.IsNullOrEmpty(obj.Role))
+                {
+                    obj.Role = SD.RoleCustomer;
+                }
+                assignRole = await _authService.AssignRoleAsync(obj);
+                if(assignRole != null && assignRole.IsSuccess)
+                {
+                    TempData["success"] = "Registration Successfull";
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+
+
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem{Text=SD.RoleAdmin,Value=SD.RoleAdmin},
+                new SelectListItem{Text=SD.RoleCustomer,Value=SD.RoleCustomer}
+            };
+            ViewBag.RoleList = roleList;
+
+            return View(obj);
+        }
+
         [HttpGet]
         public IActionResult Logout()
         {
