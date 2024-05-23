355e4beb88b2ab71dad72297966b0b239e569135
curs3m4rk <pujyapreyans35@gmail.com>
2024-05-24 00:13:32 +0530

    Product API | Assignment 2 | Added IProduct Service, Product Service, Product Controller. ProductIndex View, Product API url & Dependency Injection

---
+++ Chatora.Web/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Chatora.Web.Models;
+using Chatora.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Chatora.Web.Controllers
+{
+	public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDto>? products = new();
+
+            var response = await _productService.GetAllProductsAsync();
+
+            if (response != null && response.IsSuccess)
+            {
+                products = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return View(products);
+        }
+    }
+}
---
+++ Chatora.Web/Service/IService/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using Chatora.Web.Models;
+
+namespace Chatora.Web.Service.IService
+{
+    public interface IProductService
+    {
+        Task<ResponseDto?> GetAllProductsAsync();
+        Task<ResponseDto?> GetProductByIdAsync(int id);
+        Task<ResponseDto?> GetProductAsync(string productName);
+        Task<ResponseDto?> CreateProductAsync(ProductDto product);
+        Task<ResponseDto?> UpdateProductAsync(ProductDto product);
+        Task<ResponseDto?> DeleteProductAsync(int id);
+    }
+}
