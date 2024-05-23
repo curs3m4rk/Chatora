800590ffcc829815a4906f212f23774adede4187
curs3m4rk <pujyapreyans35@gmail.com>
2024-05-23 22:48:01 +0530

    Product API | Assignment 1 | Setup Product API | Database, DbContext, Seed Product Table, Setup Automapper | CRUD in Product API | Setup Authentication and Authorization in API

---
+++ Chatora.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Chatora.Services.ProductAPI.Models;
+using Chatora.Services.ProductAPI.Models.Dto;
+
+namespace Chatora.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>().ReverseMap();
+            });
+            return mappingConfig;
+        }
+    }
+}
