5a2232ca7fb68d42bb4b8d1e5d1ea2526711f52b
curs3m4rk <pujyapreyans35@gmail.com>
2024-05-26 22:54:25 +0530

    Shopping Cart | Cart Upsert Logic 1

--- Chatora.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
+++ Chatora.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -1,7 +1,9 @@
 using AutoMapper;
 using Chatora.Services.ShoppingCartAPI.Data;
+using Chatora.Services.ShoppingCartAPI.Models;
 using Chatora.Services.ShoppingCartAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Chatora.Services.ShoppingCartAPI.Controllers
 {
@@ -23,7 +25,43 @@ namespace Chatora.Services.ShoppingCartAPI.Controllers
         [HttpPost("CartUpsert")]
         public async Task<ResponseDto> CartUpsert(CartDto cartDto)
         {
-            
+            try
+            {
+                var cartHeaderFromDb =
+                    await _db.CartHeaders.FirstOrDefaultAsync(x => x.UserId == cartDto.CartHeader.UserId);
+                if (cartHeaderFromDb == null)
+                {
+                    // create header and details
+                    CartHeader cartHeader = _mapper.Map<CartHeader>(cartDto.CartHeader);
+                    _db.CartHeaders.Add(cartHeader);
+                    await _db.SaveChangesAsync();
+
+                    cartDto.CartDetails.First().CartHeaderId = cartHeader.CartHeaderId;
+                    _db.CartDetails.Add(_mapper.Map<CartDetails>(cartDto.CartDetails.First()));
+                    await _db.SaveChangesAsync();
+                }
+                else
+                {
+                    // if header is not null
+                    // check if details has same product
+                    var cartDetailsFromDb = await _db.CartDetails.FirstOrDefaultAsync(
+                        x => x.ProductId == cartDto.CartDetails.First().ProductId &&
+                             x.CartHeaderId == cartHeaderFromDb.CartHeaderId);
+                    if (cartDetailsFromDb == null)
+                    {
+                        // create cart details
+                    }
+                    else
+                    {
+                        // update count in cart details
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message;
+                _response.IsSuccess = false;
+            }
         }
     }
 }
