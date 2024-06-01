5d86ee60389fcae00492eb59475d4b583c985ae4
curs3m4rk <pujyapreyans35@gmail.com>
2024-06-02 01:44:34 +0530

    Bug Fix | Cart Details was not being sent to Service Bus

--- Chatora.Web/Controllers/CartController.cs
+++ Chatora.Web/Controllers/CartController.cs
@@ -50,7 +50,9 @@ public class CartController : Controller
     [HttpPost]
     public async Task<IActionResult> EmailCart(CartDto cartDto)
     {
-        ResponseDto? response = await _cartService.EmailCart(cartDto);
+        CartDto cart = await LoadCartDtoBasedOnLoggedInUser();
+        cart.CartHeader.Email = User.Claims.Where(x => x.Type == JwtRegisteredClaimNames.Email)?.FirstOrDefault()?.Value;
+        ResponseDto? response = await _cartService.EmailCart(cart);
         
         if (response != null && response.IsSuccess)
         {
