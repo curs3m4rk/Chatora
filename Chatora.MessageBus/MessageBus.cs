3306298aafe7685d838d22162a9ce4b05ab78c96
curs3m4rk <pujyapreyans35@gmail.com>
2024-06-02 01:10:43 +0530

    Message Bus Implementation

--- Chatora.MessageBus/MessageBus.cs
+++ Chatora.MessageBus/MessageBus.cs
@@ -1,6 +1,26 @@
+using System.Text;
+using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+
 namespace Chatora.MessageBus;
 
-public class MessageBus
+public class MessageBus : IMessageBus
 {
-    
+    private string connectionString =
+        "Endpoint=sb://pujyapreyans.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=U8RchpUeNsraJV4UCs9rtCKr5ouCpr78k+ASbAlGnrU=";
+    public async Task PublishMessage(object message, string topicQueueName)
+    {
+        await using var client = new ServiceBusClient(connectionString);
+
+        ServiceBusSender sender = client.CreateSender(topicQueueName);
+
+        var jsonMessage = JsonConvert.SerializeObject(message);
+        ServiceBusMessage finalMessage = new ServiceBusMessage(Encoding.UTF8.GetBytes(jsonMessage))
+        {
+            CorrelationId = Guid.NewGuid().ToString()
+        };
+
+        await sender.SendMessageAsync(finalMessage);
+        await client.DisposeAsync();
+    }
 }
