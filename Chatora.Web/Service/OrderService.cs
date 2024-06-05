84b39e02b2aefaa84faeeebf1910ff385e47626e
curs3m4rk <pujyapreyans35@gmail.com>
2024-06-05 09:08:43 +0530

    OrderAPI | Create Service in Web Project, Added service registration and URL

---
+++ Chatora.Web/Service/OrderService.cs
@@ -0,0 +1,25 @@
+﻿using Chatora.Web.Models;
+using Chatora.Web.Service.IService;
+using Chatora.Web.Utility;
+
+namespace Chatora.Web.Service
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IBaseService _baseService;
+
+        public OrderService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+        public async Task<ResponseDto?> CreateOrderAsync(CartDto cartDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = cartDto,
+                Url = SD.CouponAPIBase + "/api/order/CreateOrder"
+            });
+        }
+    }
+}
