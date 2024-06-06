fa64aa6a79b61ab846c8a0a7e0659299573a8303
curs3m4rk <pujyapreyans35@gmail.com>
2024-06-07 01:27:29 +0530

    RewardsAPI | Reward Service to process messages from Topic

---
+++ Chatora.Services.RewardsAPI/Message/RewardsMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace Chatora.Services.RewardsAPI.Message
+{
+    public class RewardsMessage
+    {
+        public string UserId { get; set; }
+        public int OrderId { get; set; }
+        public int RewardsActivity { get; set; }
+    }
+}
