efb9f9c241ef5fc68f163eae6d7ba68de0e3f88f
curs3m4rk <pujyapreyans35@gmail.com>
2024-06-07 20:22:36 +0530

    OM | Added endpoint to get list of orders in Data Table using Ajax in js function

--- Chatora.Web/Controllers/OrderController.cs
+++ Chatora.Web/Controllers/OrderController.cs
@@ -1,12 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Chatora.Web.Models;
+using Chatora.Web.Service.IService;
+using Chatora.Web.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace Chatora.Web.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
         public IActionResult OrderIndex()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            IEnumerable<OrderHeaderDto> list;
+            string userId = "";
+            if(!User.IsInRole(SD.RoleAdmin))
+            {
+                userId = User.Claims.Where(x => x.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+            }
+            ResponseDto response = _orderService.GetAllOrder(userId).GetAwaiter().GetResult();
+            if(response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<OrderHeaderDto>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                list = new List<OrderHeaderDto>();
+            }
+
+            return Json(new {data = list});
         }
     }
 }
---
+++ Chatora.Web/wwwroot/js/order.js
@@ -0,0 +1,11 @@
+﻿var dataTable;
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: "/order/GetAll" },
+        "columns": [
+            { data: 'orderHeaderId', "width": "5%" },
+            { data: 'email', "width": "25%" }
+        ]
+    })
+}
