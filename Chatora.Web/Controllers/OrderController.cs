478e61b920c8459814c5365bafda0553cb5c845e
curs3m4rk <pujyapreyans35@gmail.com>
2024-06-07 20:00:19 +0530

    OM | Added Order List UI

---
+++ Chatora.Web/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Chatora.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult OrderIndex()
+        {
+            return View();
+        }
+    }
+}
