e57fd141aaf57d7141c24f1f98b8535b4a93b06a
curs3m4rk <pujyapreyans35@gmail.com>
2024-06-07 19:43:27 +0530

    OM | Update Order Status Endpoint

--- Chatora.Services.OrderAPI/Controllers/OrderAPIController.cs
+++ Chatora.Services.OrderAPI/Controllers/OrderAPIController.cs
@@ -37,47 +37,19 @@ namespace Chatora.Services.OrderAPI.Controllers
         }
 
         [Authorize]
-        [HttpPost("CreateOrder")]
-        public async Task<ResponseDto> CreateOrder([FromBody] CartDto cartDto)
-        {
-            try
-            {
-                OrderHeaderDto orderHeaderDto = _mapper.Map<OrderHeaderDto>(cartDto.CartHeader);
-                orderHeaderDto.OrderTime = DateTime.Now;
-                orderHeaderDto.Status = SD.Status_Pending;
-                orderHeaderDto.OrderDetails = _mapper.Map<IEnumerable<OrderDetailsDto>>(cartDto.CartDetails);
-
-                OrderHeader orderCreated = _db.OrderHeaders.Add(_mapper.Map<OrderHeader>(orderHeaderDto)).Entity;
-                await _db.SaveChangesAsync();
-
-                orderHeaderDto.OrderHeaderId = orderCreated.OrderHeaderId;
-
-                _response.Result = orderHeaderDto;
-
-            }
-            catch (Exception ex)
-            {
-                _response.IsSuccess = false;
-                _response.Message = ex.Message;
-            }
-
-            return _response;
-        }
-
-        [Authorize]
         [HttpGet("GetOrders")]
         public ResponseDto? Get(string? userId = "")
         {
             try
             {
                 IEnumerable<OrderHeader> objList;
-                if(User.IsInRole(SD.RoleAdmin))
+                if (User.IsInRole(SD.RoleAdmin))
                 {
                     objList = _db.OrderHeaders.Include(x => x.OrderDetails).OrderByDescending(x => x.OrderHeaderId).ToList();
                 }
                 else
                 {
-                    objList = _db.OrderHeaders.Include(x=>x.OrderDetails).Where(x => x.UserId == userId).OrderByDescending(x => x.OrderHeaderId).ToList();
+                    objList = _db.OrderHeaders.Include(x => x.OrderDetails).Where(x => x.UserId == userId).OrderByDescending(x => x.OrderHeaderId).ToList();
                 }
 
                 _response.Result = _mapper.Map<IEnumerable<OrderHeaderDto>>(objList);
@@ -99,6 +71,35 @@ namespace Chatora.Services.OrderAPI.Controllers
             {
                 OrderHeader orederHeader = _db.OrderHeaders.Include(x => x.OrderDetails).First(x => x.OrderHeaderId == id);
                 _response.Result = _mapper.Map<OrderHeaderDto>(orederHeader);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+
+        [Authorize]
+        [HttpPost("CreateOrder")]
+        public async Task<ResponseDto> CreateOrder([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                OrderHeaderDto orderHeaderDto = _mapper.Map<OrderHeaderDto>(cartDto.CartHeader);
+                orderHeaderDto.OrderTime = DateTime.Now;
+                orderHeaderDto.Status = SD.Status_Pending;
+                orderHeaderDto.OrderDetails = _mapper.Map<IEnumerable<OrderDetailsDto>>(cartDto.CartDetails);
+
+                OrderHeader orderCreated = _db.OrderHeaders.Add(_mapper.Map<OrderHeader>(orderHeaderDto)).Entity;
+                await _db.SaveChangesAsync();
+
+                orderHeaderDto.OrderHeaderId = orderCreated.OrderHeaderId;
+
+                _response.Result = orderHeaderDto;
+
             }
             catch (Exception ex)
             {
@@ -217,5 +218,40 @@ namespace Chatora.Services.OrderAPI.Controllers
             return _response;
         }
 
+        [Authorize]
+        [HttpPost("UpdateOrderStatus/{orderId:int}")]
+        public async Task<ResponseDto> UpdateOrderStatus(int orderId, [FromBody] string newStatus)
+        {
+            try
+            {
+                OrderHeader orderHeader = _db.OrderHeaders.First(x=>x.OrderHeaderId == orderId);
+                if(orderHeader != null)
+                {
+                    if(newStatus == SD.Status_Cancelled)
+                    {
+                        // we will give refund
+                        var options = new RefundCreateOptions
+                        {
+                            Reason = RefundReasons.RequestedByCustomer,
+                            PaymentIntent = orderHeader.PaymentIntentId
+                        };
+
+                        var service = new RefundService();
+                        Refund refund = service.Create(options);
+                        orderHeader.Status = newStatus;
+                    }
+
+                    orderHeader.Status = newStatus;
+                    _db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message=ex.Message;
+            }
+
+            return _response;
+        }
     }
 }
