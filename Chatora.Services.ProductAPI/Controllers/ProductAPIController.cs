49bab0b53aa7202bf0910d16693dd9d64bf107be
curs3m4rk <pujyapreyans35@gmail.com>
2024-06-08 13:48:12 +0530

    Images | Post Endpoint to upload image in while Creating Product

--- Chatora.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Chatora.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -78,11 +78,25 @@ namespace Chatora.Services.ProductAPI.Controllers
         {
             try
             {
-                Product obj = _mapper.Map<Product>(productDto);
-                _db.Products.Add(obj);
+                Product product = _mapper.Map<Product>(productDto);
+                _db.Products.Add(product);
                 _db.SaveChanges();
 
-                _response.Result = _mapper.Map<ProductDto>(obj);
+                if(productDto.Image != null)
+                {
+                    string fileName = product.ProductId + Path.GetExtension(productDto.Image.FileName);
+                    string filePath = @"wwwroot\ProductImages" + fileName;
+                    var filePathDirectory = Path.Combine(Directory.GetCurrentDirectory(), filePath);
+                    using (var fileStream = new FileStream(filePathDirectory, FileMode.Create))
+                    {
+                        productDto.Image.CopyTo(fileStream);
+                    }
+                    var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.Value}{HttpContext.Request.PathBase.Value}";
+                    product.ImageUrl = baseUrl + "/ProductImages/" + filePath;
+                    product.ImageLocalPath = filePath;
+                }
+
+                _response.Result = _mapper.Map<ProductDto>(product);
             }
             catch(Exception ex)
             {
