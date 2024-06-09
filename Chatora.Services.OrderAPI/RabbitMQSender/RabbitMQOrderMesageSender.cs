6de929b42cd5dcf2920363003a40b81337b81261
curs3m4rk <pujyapreyans35@gmail.com>
2024-06-09 18:13:56 +0530

    RabbitMQ | Modified Sender to send to binded queue via Direct Exchange.

--- Chatora.Services.OrderAPI/RabbitMQSender/RabbitMQOrderMesageSender.cs
+++ Chatora.Services.OrderAPI/RabbitMQSender/RabbitMQOrderMesageSender.cs
@@ -6,13 +6,15 @@ namespace Chatora.Services.ShoppingCartAPI.RabbitMQSender
 {
     public class RabbitMQOrderMesageSender : IRabbitMQOrderMessageSender
     {
+        private readonly IConfiguration _configuration;
         private readonly string _hostName;
         private readonly string _userName;
         private readonly string _password;
         private IConnection _connection;
 
-        public RabbitMQOrderMesageSender()
+        public RabbitMQOrderMesageSender(IConfiguration configuration)
         {
+            _configuration = configuration;
             _hostName = "localhost";
             _userName = "guest";
             _password = "guest";
@@ -24,10 +26,17 @@ namespace Chatora.Services.ShoppingCartAPI.RabbitMQSender
             {
                 using var channel = _connection.CreateModel();
                 channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout, durable:false);
+                channel.QueueDeclare(_configuration.GetValue<string>("TopicAndQueueNames:OrderCreated_RewardsUpdateQueue"), false, false, false, null);
+                channel.QueueDeclare(_configuration.GetValue<string>("TopicAndQueueNames:OrderCreated_EmailUpdateQueue"), false, false, false, null);
+
+                channel.QueueBind(_configuration.GetValue<string>("TopicAndQueueNames:OrderCreated_RewardsUpdateQueue"), exchangeName, "RewardsUpdate");
+                channel.QueueBind(_configuration.GetValue<string>("TopicAndQueueNames:OrderCreated_EmailUpdateQueue"), exchangeName, "EmailUpdate");
+
                 var json = JsonConvert.SerializeObject(message);
                 var body = Encoding.UTF8.GetBytes(json);
 
-                channel.BasicPublish(exchange: exchangeName, "", null, body: body);
+                channel.BasicPublish(exchange: exchangeName, "RewardsUpdate", null, body: body);
+                channel.BasicPublish(exchange: exchangeName, "EmailUpdate", null, body: body);
             }
         }
 
