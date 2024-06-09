81715fb8ddbda6f101cd27a85b0410e63144c932
curs3m4rk <pujyapreyans35@gmail.com>
2024-06-09 14:28:45 +0530

    Rabbit MQ Basic Setup

---
+++ Chatora.Services.AuthAPI/RabbitMQSender/RabbitMQAuthMesageSender.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Chatora.Services.AuthAPI.RabbitMQSender
+{
+    public class RabbitMQAuthMesageSender : IRabbitMQAuthMessageSender
+    {
+        private readonly string _hostName;
+        private readonly string _userName;
+        private readonly string _password;
+        private IConnection _connection;
+
+        public RabbitMQAuthMesageSender()
+        {
+            _hostName = "localhost";
+            _userName = "guest";
+            _password = "guest";
+        }
+
+        public void SendMessage(object message, string queueName)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = _hostName,
+                UserName = _userName,
+                Password = _password
+            };
+
+            // establish connection to RabbitMQ
+            _connection = factory.CreateConnection();
+
+            using var channel = _connection.CreateModel();
+            channel.QueueDeclare(queueName);
+            var json = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(json);
+
+            channel.BasicPublish(exchange:"", routingKey: queueName, body: body);
+        }
+    }
+}
